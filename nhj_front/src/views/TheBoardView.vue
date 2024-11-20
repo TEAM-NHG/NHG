5de9cdcb580057e9ce600ce8fd41aa26758c61db
kkomeang <ghaehfdl@naver.com>
2024-11-20 18:01:33 +0900

    🔥 문구 삭제

--- nhj_front/src/views/TheBoardView.vue
+++ nhj_front/src/views/TheBoardView.vue
@@ -1,12 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="container text-center mt-3">
-    <p class="alert alert-info" role="alert">같이 떠나볼까요? 
-      <strong style="font-weight: bold;">우리</strong>
-      의 이야기를 만들어봐요.</p>
     <router-view></router-view>
-  </div>
 </template>
 
 <style>
