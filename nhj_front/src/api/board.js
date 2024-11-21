6d459c94020a1d2c1994f46afab99d042352d19b
deokjin25 <ghaehfdl7@naver.com>
2024-11-21 17:51:09 +0900

    ✨ 댓글호출 함수 추가

--- nhj_front/src/api/board.js
+++ nhj_front/src/api/board.js
@@ -10,6 +10,10 @@ function detailArticle(articleno, success, fail) {
   local.get(`/companion-board/${articleno}`).then(success).catch(fail);
 }
 
+async function detailComments(articleno, success, fail) {
+  await local.get(`/companion-board/${articleno}/comment`).then(success).catch(fail);
+}
+
 function registArticle(article, success, fail) {
   console.log("boardjs article", JSON.stringify(article));
   local.post(`/companion-board`, JSON.stringify(article)).then(success).catch(fail);
@@ -31,6 +35,7 @@ function deleteArticle(articleno, success, fail) {
 export {
   listArticle,
   detailArticle,
+  detailComments,
   registArticle,
   getModifyArticle,
   modifyArticle,
