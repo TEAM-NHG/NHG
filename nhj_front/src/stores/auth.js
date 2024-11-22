8a91692dd85338043946d99d5adc945030a92194
deokjin25 <ghaehfdl7@naver.com>
2024-11-22 17:47:15 +0900

    🚧 이미지 수정반영 작업중

--- nhj_front/src/stores/auth.js
+++ nhj_front/src/stores/auth.js
@@ -23,8 +23,8 @@ export const useAuthStore = defineStore('auth', {
     login(user) {
       // 로그인 처리: 세션 스토리지에 저장하고 상태 업데이트\
       this.isLoggedIn = true;
-      this.user = JSON.parse(user)
-      this.user.img = "http://localhost" + this.user.img
+      this.user = user
+      this.user.img = this.user.img ? "http://localhost" + user.img : ''
       sessionStorage.setItem('user', JSON.stringify(this.user));
     },
     logout() {
@@ -39,7 +39,7 @@ export const useAuthStore = defineStore('auth', {
           headers: { "Content-Type": "multipart/form-data" },
         });
         sessionStorage.setItem('user', JSON.stringify(JSON.parse(user.get('member'))));
-        this.user = JSON.parse(sessionStorage.getItem('user'))
+        this.user = JSON.parse(user.get('member'))
       }catch{
         console.log('error 발생')
       }
